2906b5f852d7c6d468463d57d94ae4086011d9bb
Louis Phillips <Louis2.Phillips@live.uwe.ac.uk>
2023-02-04 16:28:02 +0000

    push

--- GameJamProject/Assets/ResetGame.cs
+++ GameJamProject/Assets/ResetGame.cs
@@ -10,10 +10,14 @@ public class ResetGame : MonoBehaviour
     [SerializeField] private ParallaxMove mover;
     [SerializeField] private Image whiteOut;
     private bool fade = false;
+    [SerializeField] private GameObject manager;
+    private GameObject[] layerSpawnable;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+
+        manager = GameObject.FindGameObjectWithTag("Manager");
 
     }
     void Update()
@@ -39,6 +43,12 @@ public class ResetGame : MonoBehaviour
 
     IEnumerator Fade()
     {
+        /*layerSpawnable = GameObject.FindGameObjectsWithTag("Spawnable");
+        for (int i = 0; i < layerSpawnable.Length; i++)
+        {
+            Destroy(layerSpawnable[i]);
+        }*/
+        manager.GetComponent<Obsticle_spawner_script>().zoneTimer = 0f;
         fade = true;
         yield return new WaitForSeconds(2f);
         player.transform.position = spawn.transform.position;
